fabfe022681fcb424f5d042f98ac2eb0e12b8487
almirai <live.almirai@outlook.com>
2025-11-23 02:08:25 +0800

    feat(reference): add mime type impl
    Feat:
    - add project EveConv.MimeType

---
+++ Reference/EveConv.MimeType/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EveConv.Abstraction;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EveConv.MimeType
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddMimeTypeDetection(this IServiceCollection services)
+        {
+            services.AddSingleton<IMimeTypeDetection, MimeTypesDetection>();
+            return services;
+        }
+    }
+}
