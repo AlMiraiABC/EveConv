1a77c9d9fb48ba5b7c9faee0bcbf86757092dc2c
AlMirai <almirai.live@outlook.com>
2025-12-12 14:56:00 +0800

    refactor(abstraction): change StreamableFileContent to record
    Break:
    - change StreamableFileContent to record.
    - Change StreamableFileContent.LastWrite default value to UtcNow.
    - Resort StreamableFileContent constructor that asyncStreamDelegate is required.

--- EveConv.Abstraction/FileStorage/StreamableFileContent.cs
+++ EveConv.Abstraction/FileStorage/StreamableFileContent.cs
@@ -7,27 +7,53 @@ using System.Threading.Tasks;
 
 namespace EveConv.Abstraction.FileStorage;
 
-public sealed class StreamableFileContent : IDisposable
+public record StreamableFileContent : IDisposable
 {
     private Stream? _stream;
 
+    /// <summary>
+    /// Name of file.
+    /// </summary>
     public string FileName { get; } = string.Empty;
+    /// <summary>
+    /// Size of file in bytes, known as Content-Length.
+    /// </summary>
     public long FileSize { get; } = 0;
+    /// <summary>
+    /// File type, known as Content-Type or Mime-Type.
+    /// </summary>
     public string FileType { get; } = string.Empty;
-    public DateTimeOffset LastWrite { get; } = default;
+    /// <summary>
+    /// Last write datetime in UTC. Default to <see cref="DateTimeOffset.UtcNow"/>.
+    /// </summary>
+    public DateTimeOffset LastWrite { get; } = DateTimeOffset.UtcNow;
+    /// <summary>
+    /// Callback to get content stream asynchronously.
+    /// </summary>
     public Func<Task<Stream>> GetStreamAsync { get; }
 
+    /// <summary>
+    /// Inistializes a new empty instance of <see cref="StreamableFileContent"/>.
+    /// </summary>
     public StreamableFileContent()
     {
         this.GetStreamAsync = () => Task.FromResult<Stream>(new MemoryStream());
     }
 
+    /// <summary>
+    /// Initializes a new instance of <see cref="StreamableFileContent"/> with specified props.
+    /// </summary>
+    /// <param name="fileName">File name. Cannot be null or whitespace.</param>
+    /// <param name="fileSize">File size in bytes, known as Content-Length.</param>
+    /// <param name="asyncStreamDelegate">Callback to get content stream asynchronously. Cannot be null.</param>
+    /// <param name="fileType">File type, known as Content-Type or Mime-Type.</param>
+    /// <param name="lastWriteTimeUtc">Last write datetime in UTC. Default to now.</param>
     public StreamableFileContent(
         string fileName,
         long fileSize,
+        Func<Task<Stream>> asyncStreamDelegate,
         string fileType = "application/octet-stream",
-        DateTimeOffset lastWriteTimeUtc = default,
-        Func<Task<Stream>>? asyncStreamDelegate = null)
+        DateTimeOffset lastWriteTimeUtc = default)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(fileName);
         ArgumentNullException.ThrowIfNull(asyncStreamDelegate);
@@ -35,7 +61,7 @@ public sealed class StreamableFileContent : IDisposable
         this.FileName = fileName;
         this.FileSize = fileSize;
         this.FileType = fileType;
-        this.LastWrite = lastWriteTimeUtc;
+        this.LastWrite = lastWriteTimeUtc == default ? DateTimeOffset.UtcNow : lastWriteTimeUtc;
         this.GetStreamAsync = async () =>
         {
             this._stream = await asyncStreamDelegate().ConfigureAwait(false);
