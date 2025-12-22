4c083f781bfd75d727548b4853a4edbff63238d5
almirai <live.almirai@outlook.com>
2025-12-22 21:43:12 +0800

    refactor(abstraction): simpilify idocumentparser
    Break:
    - remove ParserAsync overload of string, uri and byte array
    
    Feat:
    - add prop Priority

--- EveConv.Abstraction/DocParser/IDocumentParser.cs
+++ EveConv.Abstraction/DocParser/IDocumentParser.cs
@@ -10,6 +10,11 @@ namespace EveConv.Abstraction.DocParser
     public interface IDocumentParser
     {
         /// <summary>
+        /// Priority of the document parser. Lower values indicate higher priority.
+        /// </summary>
+        public int Priority { get; }
+
+        /// <summary>
         /// Determines whether the specified file type is accepted by the current filter.
         /// </summary>
         /// <param name="fileType">The file extension or media type.</param>
@@ -19,48 +24,10 @@ namespace EveConv.Abstraction.DocParser
         /// <summary>
         /// Asynchronously parses the specified file stream to document.
         /// </summary>
+        /// <param name="source">Original file source.</param>
         /// <param name="fileStream">The specified file stream.</param>
         /// <param name="cancellationToken">Task cancellation token.</param>
         /// <returns>A task that represents the asynchronous operation that contains a structured document.</returns>
-        async Task<Document> ParseAsync(Stream fileStream, CancellationToken cancellationToken = default)
-        {
-            using var ms = new MemoryStream();
-            await fileStream.CopyToAsync(ms, cancellationToken);
-            return await ParseAsync(ms.ToArray(), cancellationToken);
-        }
-
-        /// <summary>
-        /// Asynchronously parses the specified file path(on local machine) to document.
-        /// </summary>
-        /// <param name="filePath">The specified file path.</param>
-        /// <param name="cancellationToken">Task cancellation token.</param>
-        /// <returns>A task that represents the asynchronous operation that contains a structured document.</returns>
-        /// <exception cref="FileNotFoundException">The specified file path is not exist.</exception>
-        async Task<Document> ParseAsync(string filePath, CancellationToken cancellationToken = default)
-        {
-            ArgumentException.ThrowIfNullOrWhiteSpace(filePath);
-            if (!File.Exists(filePath))
-            {
-                throw new FileNotFoundException("File not found on local machine", filePath);
-            }
-            using var fs = File.OpenRead(filePath);
-            return await ParseAsync(fs, cancellationToken);
-        }
-
-        /// <summary>
-        /// Asynchronously parses the specified file URI to document.
-        /// </summary>
-        /// <param name="fileUri">The specified file URI.</param>
-        /// <param name="cancellationToken">Task cancellation token.</param>
-        /// <returns>A task that represents the asynchronous operation that contains a structured document.</returns>
-        Task<Document> ParseAsync(Uri fileUri, CancellationToken cancellationToken = default);
-
-        /// <summary>
-        /// Asynchronously parses the specified file data to document.
-        /// </summary>
-        /// <param name="fileData">The specified file data that read to bytes.</param>
-        /// <param name="cancellationToken">Task cancellation token.</param>
-        /// <returns>A task that represents the asynchronous operation that contains a structured document.</returns>
-        Task<Document> ParseAsync(byte[] fileData, CancellationToken cancellationToken = default);
+        Task<Document> ParseAsync(string source, Stream fileStream, CancellationToken cancellationToken = default);
     }
 }
