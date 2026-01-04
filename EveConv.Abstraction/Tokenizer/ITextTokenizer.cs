beefee1f29f176733b0b2ba57e5bcb648b60f675
almirai <live.almirai@outlook.com>
2026-01-04 23:45:38 +0800

    feat(abstraction): add tokenizer interfaces

---
+++ EveConv.Abstraction/Tokenizer/ITextTokenizer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EveConv.Abstraction.Tokenizer
+{
+    public interface ITextTokenizer<V> : ITokenizer<V, string>
+    {
+    }
+}
---
+++ EveConv.Abstraction/Tokenizer/ITokenizer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EveConv.Abstraction.Tokenizer
+{
+    public interface ITokenizer
+    {
+    }
+
+    /// <summary>
+    /// Generic interface for tokenizer models.
+    /// </summary>
+    /// <typeparam name="V"></typeparam>
+    /// <typeparam name="I"></typeparam>
+    public interface ITokenizer<V, I>
+    {
+        /// <summary>
+        /// Asynchronously tokenizes the specified input value.
+        /// </summary>
+        /// <param name="input">The specified input.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <returns>A task that contains tokenize result.</returns>
+        Task<V[]> TokenizeAsync(I input, IDictionary<string, object>? context = null, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Asynchronously tokenizes the specified batch of input values.
+        /// </summary>
+        /// <param name="inputs">The specified inputs.</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<V[][]> TokenizeBatchAsync(I[] inputs, IDictionary<string, object>? context = null, CancellationToken cancellationToken = default);
+    }
+}
