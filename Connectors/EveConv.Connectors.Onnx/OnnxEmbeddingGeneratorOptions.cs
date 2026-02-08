118894b8ced27510fd965aeb23e917f00c6cf737
AlMirai <almirai.live@outlook.com>
2026-02-08 21:40:56 +0800

    style(connectors): format code

--- Connectors/EveConv.Connectors.Onnx/OnnxEmbeddingGeneratorOptions.cs
+++ Connectors/EveConv.Connectors.Onnx/OnnxEmbeddingGeneratorOptions.cs
@@ -8,7 +8,7 @@ namespace EveConv.Connectors.Onnx
 {
     public class OnnxEmbeddingGeneratorOptions : IOptions<OnnxEmbeddingGeneratorOptions>
     {
-        
+
 
         OnnxEmbeddingGeneratorOptions IOptions<OnnxEmbeddingGeneratorOptions>.Value => this;
 
